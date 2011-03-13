c40466750a8ef155d18df6085ec184a798c7bb73
stillinbeta <web@stillinbeta.com>
2011-03-13 19:32:15 -0400

    Basic outline for android app

---
+++ android/src/com/stillinbeta/utordroid/UTORDroid.java
@@ -0,0 +1,16 @@
+package com.stillinbeta.utordroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class UTORDroid extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
