92bf220452046282626294e112ddd51e58813134
stillinbeta <web@stillinbeta.com>
2011-03-13 04:27:45 -0400

    Program properly retrievs fields

--- main.java
+++ main.java
@@ -1,38 +1,84 @@
 import org.ccil.cowan.tagsoup.Parser;
 import org.xml.sax.XMLReader;
 import org.xml.sax.InputSource;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.dom.DOMResult;
 import java.net.URL;
+import java.util.HashMap;
+import java.net.HttpURLConnection;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Attributes;
+import org.xml.sax.XMLReader;
 
-class Main {
+class Main extends DefaultHandler{
+    private boolean inForm;
+    private boolean formParsed;
+    private HashMap<String,String> fields;
+
+    public Main() {
+        this.inForm = false;
+        this.formParsed = false;
+        this.fields = new HashMap<String,String>();
+    }
+
     public static void main(String[] args){
         URL url;
         try {
-            url = new URL("http://www.stillinbeta.com");
+            url = new URL("http://login.wireless.utoronto.ca");
         }
         catch (MalformedURLException e) {
             System.out.println("Malformed URL");
             return;
         }
-        XMLReader reader;
-        Transformer transformer;
-        DOMResult result;
+        String newURL;  
         try {
-            reader = new Parser();
-            reader.setFeature(Parser.namespacesFeature, false);
-            reader.setFeature(Parser.namespacePrefixesFeature, false);
-            transformer = TransformerFactory.newInstance().newTransformer();
-            result = new DOMResult();
-            transformer.transform(new SAXSource(reader, 
-                        new InputSource(url.openStream())), result);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.connect();
+            newURL = conn.getHeaderField("Location");
         }
         catch (Exception e) {
-            System.out.println("Something went wrong");
-            return;  
+            System.out.println("Couldn't Retrieve Redirect URL");
+            return; 
+        }
+
+        try {
+            url = new URL(newURL);
+        }
+        catch (MalformedURLException e) {
+            System.out.println("Malformed URL");
+            return;
+        }
+
+        Main handler = new Main();
+        XMLReader xr = new Parser();
+        xr.setContentHandler(handler);
+        xr.setErrorHandler(handler);    
+
+        try {
+            xr.parse(new InputSource(url.openStream()));
+            System.out.println(handler.fields);
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong"+e.toString());
+        }
+
+    } 
+
+    public void startElement (String uri, String name,
+                      String qName, Attributes atts) {
+        if (!this.formParsed && name.compareTo("form") == 0) {
+            this.inForm = true;
+        }
+        if ( this.inForm && name.compareTo("input") == 0 &&
+            atts.getValue("type").compareTo("hidden") == 0 ) {
+            fields.put(atts.getValue("name"),atts.getValue("value"));
+        }
+    }
+
+    public void endElement (String uri, String name, String qName) {
+        if (name.compareTo("form") == 0) {
+            this.inForm = false;
+            this.formParsed = true;
         }
     }
 }
