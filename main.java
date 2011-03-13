1677bcc0bb063c8e26d41e9e56f98121d061b5e8
stillinbeta <web@stillinbeta.com>
2011-03-13 03:12:01 -0400

    Doesnt't do much anything else

--- main.java
+++ main.java
@@ -1,14 +1,15 @@
+import org.ccil.cowan.tagsoup.Parser;
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.dom.DOMResult;
 import java.net.URL;
 import java.net.MalformedURLException;
-import org.ccil.cowan.tagsoup.Parser;
-import org.jdom.input.SAXBuilder;
-import org.jdom.Document;
-import org.jdom.JDOMException;
-import org.xml.sax.XMLReader;
 
 class Main {
     public static void main(String[] args){
-        SAXBuilder builder = new SAXBuilder("org.ccil.cowan.tagsoup.Parser");
         URL url;
         try {
             url = new URL("http://www.stillinbeta.com");
@@ -17,30 +18,21 @@ class Main {
             System.out.println("Malformed URL");
             return;
         }
-        Document doc;
-        try {
-            doc = builder.build(url);
-        }
-        catch (Exception e) {
-            System.out.println("JDOM Exception: "+e.toString());
-        }
-
         XMLReader reader;
+        Transformer transformer;
+        DOMResult result;
         try {
             reader = new Parser();
             reader.setFeature(Parser.namespacesFeature, false);
             reader.setFeature(Parser.namespacePrefixesFeature, false);
+            transformer = TransformerFactory.newInstance().newTransformer();
+            result = new DOMResult();
+            transformer.transform(new SAXSource(reader, 
+                        new InputSource(url.openStream())), result);
         }
-        catch (Exception e){
-            System.out.println("Something went wrong: "+e.toString());
+        catch (Exception e) {
+            System.out.println("Something went wrong");
+            return;  
         }
-/*
-        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-
-        DOMResult result = new DOMResult();
-        transformer.transform(new SAXSource(reader, 
-            new InputSource(url.openStream())), result); */
-        System.out.println("At least it compiled");  
-        
     }
 }
